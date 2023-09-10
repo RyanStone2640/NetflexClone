9601197076d7eb9d46236010314d9d16921fa1d0
Ryan <ryan880822@gmail.com>
2023-09-10 22:31:46 +0800

    add login img

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,14 +1,16 @@
 import Image from 'next/image'
 
-
-
 export default async function Index() {
 
-  return <div className='min-h-screen bg-slate-600  overflow-clip'>
-    <Image
-      src="/loginBG.jpg"
-      fill={true}
-      sizes='(min-width: 100%)'
-      alt="Picture of the author" />
-  </div>
+  return (
+    <div className='relative min-h-screen'>
+      <div className='absolute min-h-screen h-full w-full'>
+        <img 
+          className='min-h-full min-w-full max-w-none'
+          alt='' 
+          src="/loginBG_small.jpg" 
+          srcSet="/loginBG_small.jpg 1000w, /loginBG_medium.jpg 1500w, /loginBG_large.jpg 1800w" />
+      </div>
+    </div>
+  )
 }
